ef4e37787b9c995bd37bab25d95d251cfed32e29
Serge <s@shayderov.ru>
2020-05-03 16:37:26 +0300

    Can view list of schedules

--- spec/features/schedules/view_spec.rb
+++ spec/features/schedules/view_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 feature 'User can view a schedule', %q{
   In order to be able to see my arranged events
   As an authenticated user
-  I want to be able to view a schedule
+  I want to be able to view schedules
 } do
 
-  given(:schedule) { create(:schedule) }
+  given!(:schedule) { create(:schedule) }
+  given!(:schedule2) { create(:schedule, :new) }
+
+  scenario 'User tries to view a list of schedules' do
+    visit schedules_path
+
+    expect(page).to have_content schedule.title
+    expect(page).to have_content schedule2.title
+  end
 
   scenario 'User tries to view a schedule' do
     visit schedule_path(schedule)
