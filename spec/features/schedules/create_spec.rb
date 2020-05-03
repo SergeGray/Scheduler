f1418faeeebcffaf2a06f4a075a639d585326477
Serge <s@shayderov.ru>
2020-05-03 15:30:32 +0300

    Can't create schedules with errors

--- spec/features/schedules/create_spec.rb
+++ spec/features/schedules/create_spec.rb
@@ -6,8 +6,9 @@ feature 'User can create a schedule', %q{
   I want to be able to create a schedule
 } do
 
+  background { visit new_schedule_path }
+
   scenario 'User tries to create a schedule' do
-    visit new_schedule_path
     fill_in 'Title', with: 'My schedule'
     fill_in 'Description', with: 'My own schedule for arranging events'
     click_button 'Create schedule'
@@ -15,5 +16,13 @@ feature 'User can create a schedule', %q{
     expect(page).to have_content 'Successfully created'
     expect(page).to have_content 'My schedule'
     expect(page).to have_content 'My own schedule for arranging events'
+  end
+
+  scenario 'User tries to create a schedule with invalid parameters' do
+    click_button 'Create schedule'
+
+    expect(page).to have_content '2 error(s) detected:'
+    expect(page).to have_content "Title can't be blank"
+    expect(page).to have_content "Description can't be blank"
   end
 end
