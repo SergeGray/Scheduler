2bf837f8cb6133be0b55bfad7d46beba9a02f46e
Serge <s@shayderov.ru>
2020-05-17 21:14:34 +0300

    Can't update appointments with invalid parameters

--- spec/features/appointments/edit_spec.rb
+++ spec/features/appointments/edit_spec.rb
@@ -33,5 +33,20 @@ feature 'User can edit an appointment', %q{
     expect(page).to_not have_content appointment.date
   end
 
-  scenario 'User tries to edit an appointment with invalid parameters'
+  scenario 'User tries to edit an appointment with invalid parameters' do
+    click_link 'Edit appointment'
+
+    within(".time-slot-#{time_slot.id}-appointments") do
+      select '2020', from: 'appointment_date_1i'
+      select 'December', from: 'appointment_date_2i'
+      select '14', from: 'appointment_date_3i'
+      select time_slot.day_and_time, from: 'appointment_time_slot_id'
+      click_button 'Edit appointment'
+    end
+
+    expect(page).to have_content '1 error(s) detected'
+    expect(page).to have_content(
+      'Time slot day of the week does not match the Date'
+    )
+  end
 end
