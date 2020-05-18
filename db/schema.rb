138cf0b1946bd6216e3bea0e56ab548339a78429
Serge <s@shayderov.ru>
2020-05-18 17:29:27 +0300

    User sign in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_174125) do
+ActiveRecord::Schema.define(version: 2020_05_18_142648) do
 
   create_table "appointments", force: :cascade do |t|
     t.string "title", null: false
@@ -38,6 +38,18 @@ ActiveRecord::Schema.define(version: 2020_05_17_174125) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["schedule_id"], name: "index_time_slots_on_schedule_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "appointments", "time_slots"
