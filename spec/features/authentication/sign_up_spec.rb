ec2d5ad14f2e33c9ea26926379c3a25f280ca353
Serge <s@shayderov.ru>
2020-05-18 17:31:50 +0300

    User sign up

---
+++ spec/features/authentication/sign_up_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+feature 'User can sign up', %q(
+  In order to be able to ask questions
+  As an unauthenticated user
+  I want to be able to sign up
+) do
+  given(:user) { create(:user) }
+
+  background { visit new_user_registration_path }
+
+  scenario 'User tries to sign up' do
+    fill_in 'Email', with: 'email@example.com'
+    fill_in 'Password', with: '123123'
+    fill_in 'Password confirmation', with: '123123'
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Welcome!'
+  end
+
+  scenario 'User tries to sign up with errors' do
+    click_button 'Sign up'
+
+    expect(page).to have_content "Email can't be blank"
+  end
+
+  scenario 'User tries to sign up with existing Email' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: '123123'
+    fill_in 'Password confirmation', with: '123123'
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Email has already been taken'
+  end
+
+  scenario 'User tries to sign up with wrong password confirmation' do
+    fill_in 'Email', with: 'email@example.com'
+    fill_in 'Password', with: '123123'
+    fill_in 'Password confirmation', with: '000000'
+    click_button 'Sign up'
+
+    expect(page).to have_content "Password confirmation doesn't match Password"
+  end
+end
