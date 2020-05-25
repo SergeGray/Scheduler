c65d09ab87117e9878dd557377038209cec85dc4
Serge <s@shayderov.ru>
2020-05-25 12:27:46 +0300

    Appointment destroy policy

--- spec/features/appointments/delete_spec.rb
+++ spec/features/appointments/delete_spec.rb
@@ -6,12 +6,16 @@ feature 'User can delete an appointment', %q{
   I want to be able to delete an appointment
 }, js: true do
   given(:user) { create(:user) }
-  given!(:schedule) { create(:schedule) }
+  given(:schedule_owner) { create(:user) }
+  given(:appointment_owner) { create(:user) }
+  given!(:schedule) { create(:schedule, user: schedule_owner) }
   given!(:time_slot) { create(:time_slot, schedule: schedule) }
-  given!(:appointment) { create(:appointment, time_slot: time_slot) }
+  given!(:appointment) do
+    create(:appointment, time_slot: time_slot, user: appointment_owner)
+  end
 
-  scenario 'Authenticated user tries to delete an appointment' do
-    sign_in(user)
+  scenario 'Schedule owner tries to delete an appointment' do
+    sign_in(schedule_owner)
     visit schedule_path(schedule)
 
     expect(page).to have_content appointment.date
@@ -22,6 +26,27 @@ feature 'User can delete an appointment', %q{
     within(".time-slot-#{time_slot.id}-appointments") do
       expect(page).to_not have_content appointment.date
     end
+  end
+
+  scenario 'Appointment owner tries to delete an appointment' do
+    sign_in(appointment_owner)
+    visit schedule_path(schedule)
+
+    expect(page).to have_content appointment.date
+    click_link 'Delete appointment'
+
+    expect(page).to have_content 'Successfully deleted'
+
+    within(".time-slot-#{time_slot.id}-appointments") do
+      expect(page).to_not have_content appointment.date
+    end
+  end
+
+  scenario 'Unauthenticated user tries to delete an appointment' do
+    sign_in(user)
+    visit schedule_path(schedule)
+
+    expect(page).to_not have_link 'Delete appointment'
   end
 
   scenario 'Unauthenticated user tries to delete an appointment' do
